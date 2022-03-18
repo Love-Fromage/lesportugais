2342b91bf63a2d4a94b6065b1189acbc5fa806db
mathieu <mathecool22@gmail.com>
2022-03-18 19:48:04 -0400

    carousel de placer

--- src/components/mainpage/Hero.js
+++ src/components/mainpage/Hero.js
@@ -1,30 +1,68 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import "../scss/Hero.scss";
 
 const Hero = () => {
+	const isDesktopOrLaptop = useMediaQuery({
+		query: "(min-width: 1224px)",
+	});
+	const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 	return (
 		<>
-			<div className="chapeau"></div>
-			<div className="hero">
-				<div className="cont-text-hero">
-					<h1>
-						<span>Paysagiste</span> <br />
-						<span>LesPortugais</span>
-					</h1>
-					<h1>
-						<p>On travaille</p>
-						<p>pour vous</p>
-						<p>comme si c'était</p>
-						<p>pour nous!</p>
-					</h1>
-					<Link to="/contact">
-						<a href="#" className="devis-link">
-							Devis gratuit
-						</a>
-					</Link>
-				</div>
-			</div>
+			{isTabletOrMobile && (
+				<>
+					<div className="chapeau"></div>
+					<div className="hero">
+						<div className="cont-text-hero">
+							<h1>
+								<span>Paysagiste</span> <br />
+								<span>LesPortugais</span>
+							</h1>
+							<h1>
+								<p>On travaille</p>
+								<p>pour vous</p>
+								<p>comme si c'était</p>
+								<p>pour nous!</p>
+							</h1>
+							<Link to="/contact">
+								<a href="#" className="devis-link">
+									Devis gratuit
+								</a>
+							</Link>
+						</div>
+					</div>
+				</>
+			)}
+			{isDesktopOrLaptop && (
+				<>
+					<div className="chapeau"></div>
+					<div className="cont-hero-d">
+						<div className="hero-d">
+							<div className="cont-text-hero-d">
+								<h1>
+									<span>Paysagiste</span>
+									<span>
+										<span className="Les">Les</span>
+										Portugais
+									</span>
+								</h1>
+								<h1>
+									<p>On travaille</p>
+									<p>pour vous</p>
+									<p>comme si c'était</p>
+									<p>pour nous!</p>
+								</h1>
+								<Link to="/contact">
+									<a href="#" className="devis-link-d">
+										Devis gratuit
+									</a>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</>
+			)}
 		</>
 	);
 };
--- src/components/mainpage/LeCarouselle.js
+++ src/components/mainpage/LeCarouselle.js
@@ -83,7 +83,80 @@ const LeCarouselle = () => {
 				</div>
 			)}
 
-			{isDesktopOrLaptop && <div></div>}
+			{isDesktopOrLaptop && (
+				<div>
+					<div className="cont-carousel-d">
+						<div className="cont-slider-d">
+							<Carousel controls={false}>
+								<Carousel.Item interval={speed}>
+									<img
+										className="d-block w-100 img-R"
+										src="https://www.colorhexa.com/303030.png"
+										alt="First slide"
+									/>
+									<Carousel.Caption>
+										<p
+											className="p-first"
+											id="premier-slide"
+										>
+											"Paysagiste Les Portugais <br /> are
+											a very good company. <br /> The
+											service is excellent, <br /> when
+											you need a service <br /> they are
+											there for you."
+										</p>
+										<h2>Dmitry Drachyov</h2>
+										<p>Portes de l'Europe à Longueuil</p>
+									</Carousel.Caption>
+								</Carousel.Item>
+								<Carousel.Item interval={speed}>
+									<img
+										className="d-block w-100 img-R"
+										src="https://www.colorhexa.com/303030.png"
+										alt="First slide"
+									/>
+									<Carousel.Caption>
+										<p className="p-first p-2">
+											"Nous avons eu d'excellents
+											commentaires suite aux ménage du
+											terrain fait hier. Une résidente de
+											longue date a dit que c'étais le
+											plus beau ménage de printemps
+											qu'elle a vu depuis 12 ans."
+										</p>
+										<h2>Catherine-Amélie Côté</h2>
+										<p>Boucherville</p>
+									</Carousel.Caption>
+								</Carousel.Item>
+								<Carousel.Item interval={speed}>
+									<img
+										className="d-block w-100 img-R"
+										src="https://www.colorhexa.com/303030.png"
+										alt="First slide"
+									/>
+									<Carousel.Caption>
+										<p className="p-first para-3">
+											"Depuis que nous avons retenus les
+											services de Paysagiste Les Portugais
+											il y a maintenant 5 ans, nous
+											faisons l'envie de nos voisins. Son
+											équipe a un soucis du détail
+											exceptionnel et l'allure de notre
+											entrée et de notre cours en témoigne
+											à chaque coup d’œil. <br /> Merci
+											beaucoup à Paysagiste Les
+											Portugais."
+										</p>
+										<h2>Pierre E. Jean-Felix</h2>
+										<p>Rue de Louviers à Brossard</p>
+									</Carousel.Caption>
+								</Carousel.Item>
+							</Carousel>
+						</div>
+						<div className="boute-rouge"></div>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
