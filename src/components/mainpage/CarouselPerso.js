57f622e0d4a748e1f2f786051b765b04b8173eb9
mathieu <mathecool22@gmail.com>
2022-03-21 12:16:53 -0400

    reajustement du contact  + essai carou

--- src/components/mainpage/CarouselPerso.js
+++ src/components/mainpage/CarouselPerso.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { Fade } from "react-bootstrap";
 import { GrNext } from "react-icons/gr";
 import { useMediaQuery } from "react-responsive";
+
 import image1 from "../../images/IMG_0239-2.jpg";
 import image2 from "../../images/IMG_0089-2.jpg";
 import image3 from "../../images/IMG_0215-2.jpg";
@@ -20,22 +21,41 @@ const CarouselPerso = () => {
 	const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 	const [marge, setMarge] = useState(0);
 	const [pos, setPos] = useState(0);
+	const [isActive, setIsActive] = useState(true);
+
 	let bandStyle = {
 		marginLeft: marge + "vw",
-		transtion: "ease 1s",
 	};
 
 	let bande = (
 		<div className="bande" style={bandStyle}>
-			<img src={image1} alt="" />
-			<img src={image2} alt="" />
-			<img src={image3} alt="" />
-			<img src={image4} alt="" />
-			<img src={image5} alt="" />
-			<img src={image6} alt="" />
-			<img src={image7} alt="" />
-			<img src={image8} alt="" />
-			<img src={image9} alt="" />
+			<div className="cont-inner-bande">
+				<img src={image1} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image2} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image3} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image4} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image5} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image6} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image7} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image8} alt="" />
+			</div>
+			<div className="cont-inner-bande">
+				<img src={image9} alt="" />
+			</div>
 		</div>
 	);
 	const nextSlide = () => {
@@ -48,7 +68,7 @@ const CarouselPerso = () => {
 		} else {
 			setPos(pos + 1);
 			// console.log(pos);
-			setMarge(marge - 80);
+			setMarge(marge - 60);
 		}
 	};
 	const prevSlide = () => {
@@ -56,10 +76,10 @@ const CarouselPerso = () => {
 		if (pos === 0) {
 			// console.log(pos);
 			setPos(8);
-			setMarge(-(8 * 80));
+			setMarge(-(8 * 60));
 		} else {
 			setPos(pos - 1);
-			setMarge(marge + 80);
+			setMarge(marge + 60);
 		}
 	};
 
